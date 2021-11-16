3193e013676395a482eb94778fc8eaba608bc66d
Porkoláb Ádám <adam.porkolab@gmail.com>
2021-11-16 09:20:07 +0100

    Final addings.

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,12 @@
+const converterPromise = (getArr = []) => {
+	return new Promise((resolve, reject) => {
+		try {
+			const setArr = getArr.map(item => item.toUpperCase());
+			resolve(setArr);
+		} catch {
+			reject('Error: Not all elements are string type!');
+		}
+	});
+};
 
-
-
-
-
-
-// export default converterPromise;
+export default converterPromise;
